0e1bbf30c6d2f9c64a34690e118f1643a0396c17
Smrutiworkz12 <smruti.dash@workz.com>
2026-05-06 15:28:40 +0400

    fix: updated tpage layout

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,10 +2,8 @@ import UploadFile from "./components/page";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <UploadFile />
-      </main>
-    </div>
+    <main className="min-h-screen w-full bg-[#0d1117]">
+      <UploadFile />
+    </main>
   );
 }
